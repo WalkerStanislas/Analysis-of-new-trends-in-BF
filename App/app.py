e4dbe0eb24e4a23a934cc3e500fbf7da0f769b82
Walker <walkercompaore972@gmail.com>
2025-04-09 02:08:56 +0000

    Sentiment analysis with VADER

--- App/app.py
+++ App/app.py
@@ -1,1 +1,15 @@
+# Importer les bibliothèques nécessaires
 import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
+import pyLDAvis
+import pyLDAvis.gensim_models as gensimvis
+import gensim
+from gensim.corpora import Dictionary
+from gensim.models import LdaModel
+import ast
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+#######################
